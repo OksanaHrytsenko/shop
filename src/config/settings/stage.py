c3394b7f6699b7cda6dbf9dad8bffbae6878508c
Oksana <0682112130o@gmail.com>
2023-12-06 17:45:16 +0200

    added dev , stage and prod

---
+++ src/config/settings/stage.py
@@ -0,0 +1,22 @@
+from config.settings.base_settings import *  # NOQA
+
+SECRET_KEY = "django-insecure-)811+4fn7&7y48%5#m@76sd!q373@pqi3n%agr&p%c5is@u9to"
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
